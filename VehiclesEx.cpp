151ae8e61a1240bf4e1b7f956cf12f7860299a0f
Jaxg2 <jaxon.gillham@gmail.com>
2024-08-01 11:39:38 -0500

    main

--- VehiclesEx.cpp
+++ VehiclesEx.cpp
@@ -4,6 +4,23 @@
 #include <iostream>
 using namespace std;
 
+void testBoat();
+void testCar();
+
+
+void testBoat()
+{
+	Position here(50, 75);
+	Car myCar(70, here, 4, 5);
+	Position there(120, 10);
+	cout << myCar.getETA(there) << endl << endl;
+}
+
+void testCar()
+{
+
+}
+
 int main()
 {
 
@@ -11,5 +28,11 @@ int main()
 	Car myCar(70, here, 4, 5);
 	Position there(120, 10);
 	cout << myCar.getETA(there) << endl << endl;
+
+	Vehicle* vehicles[5];
+	Position here(20, 20);
+	Position there(40, 60);
+	Position overThere(150, 400);
+	Position wayOverThere(1400, 2841);
 
 }
