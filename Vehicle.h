da685bd53efe497e728621baa96c12c593900e64
Jaxg2 <jaxon.gillham@gmail.com>
2024-08-01 10:29:54 -0500

    w

---
+++ Vehicle.h
@@ -0,0 +1,19 @@
+#ifndef VEHICLE_H
+#define VEHICLE_H
+#include "Position.h"
+#include <iostream>
+using namespace std;
+
+class Vehicle
+{
+private:
+	double speed;
+	Position position;
+	int numPassengers;
+	int maxPassengers;
+public:
+
+
+};
+
+#endif
