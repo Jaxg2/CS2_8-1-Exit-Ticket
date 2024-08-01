20830f9060c93aa9df8d5b784b7b31ff7c185b65
Jaxg2 <jaxon.gillham@gmail.com>
2024-08-01 18:03:09 -0500

    main

--- VehiclesEx.cpp
+++ VehiclesEx.cpp
@@ -1,6 +1,7 @@
 #include "Position.h"
 #include "Vehicle.h"
 #include "Car.h"
+#include "Boat.h"
 #include <iostream>
 using namespace std;
 
@@ -34,5 +35,18 @@ int main()
 	Position there(40, 60);
 	Position overThere(150, 400);
 	Position wayOverThere(1400, 2841);
+	Position reallyClose(21, 21);
+	Position theBestSpot(42, 42);
 
+	vehicles[0] = new Car(50, here, 2, 3);
+	vehicles[1] = new Boat(24, there, 4, 12);
+	vehicles[2] = new Car(75, overThere, 1, 2);
+	vehicles[3] = new Boat(10, wayOverThere, 3, 6);
+	vehicles[4] = new Car(20, reallyClose, 2, 5);
+
+	for (Vehicle* v : vehicles)
+	{
+		v->travel(theBestSpot);
+		cout << endl;
+	}
 }
